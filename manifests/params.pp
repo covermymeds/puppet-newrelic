ccf5863a21df1dbd042390feb8b1a3bb793f7434
Adam Stephens <astephens@covermymeds.com>
2017-07-17 11:12:24 -0400

    don't download the newrelic repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,11 +20,6 @@ class newrelic::params {
       $newrelic_php_package      = 'newrelic-php5'
       $newrelic_php_service      = 'newrelic-daemon'
       $newrelic_php_conf_dir     = ['/etc/php.d']
-      package { 'newrelic-repo-5-3.noarch':
-        ensure   => present,
-        source   => 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm',
-        provider => rpm,
-      }
     }
     'Debian': {
       $newrelic_package_name     = 'newrelic-sysmond'
